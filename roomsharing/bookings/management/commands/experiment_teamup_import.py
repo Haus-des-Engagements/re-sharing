79941fa24e4a5490d97d3280296b36ce9076a5cf
ToastMX <ToastMX@paulk.de>
2024-04-04 01:53:58 +0200

    feat: Teamup Importer first draft, json cache, teamup API key in .env, prettyprint outputs, 2 manage commands

---
+++ roomsharing/bookings/management/commands/experiment_teamup_import.py
@@ -0,0 +1,68 @@
+from django.core.management.base import BaseCommand
+from roomsharing.bookings.models import Booking, BookingGroup
+from django.core.files.storage import default_storage
+import json
+
+
+## you can run this with ython manage.py experiment_teamup_import
+## it reads: teamup_event_data_by_boopkinggroup.json in root
+class Command(BaseCommand):
+    help = "read media/temup-"
+
+    # def add_arguments(self, parser):
+    #     # Positional arguments
+    #     # parser.add_argument("poll_ids", nargs="+", type=int)
+
+    #     # Named (optional) arguments
+    #     parser.add_argument(
+    #         "--key",
+    #         action="store",
+    #         help="Teamup API KEY",
+    #     )
+
+    def handle(self, *args, **options):
+        import json
+        
+        sorted_events = read_latest_teamup_json()
+        if sorted_events:
+            # notice some events have multiple subcalenders/rooms
+            for group_index, group in enumerate(sorted_events):
+                if group_index >= 300:  # Limit to the first group only
+                    break
+
+                g = BookingGroup(title=sorted_events[group][0]['title'])
+                print(f"{sorted_events[group][0]['title']} ({len(sorted_events[group])} Bookings)")       
+        
+      
+
+                for booking_index, booking in enumerate(sorted_events[group]):
+                    from datetime import datetime
+                    start_dt = datetime.strptime(booking['start_dt'], '%Y-%m-%dT%H:%M:%S%z').strftime('%Y-%m-%d %H:%M')
+                    end_dt = datetime.strptime(booking['end_dt'], '%Y-%m-%dT%H:%M:%S%z').strftime('%Y-%m-%d %H:%M')
+                    booking_details = f"    {booking['id']}, {booking['title']}, {start_dt}, {end_dt}, {booking['subcalendar_id']}"
+                    print(booking_details)
+                    if booking_index >= 4:  # Limit to the first booking only
+                        break
+
+
+def read_latest_teamup_json():
+    # Liste alle 'teamup-' Dateien auf
+    all_files = default_storage.listdir('teamup')[1]  # Das listdir Argument hängt von deiner Speicherstruktur ab
+    teamup_files = [f for f in all_files if f.endswith('.json')]
+
+    if not teamup_files:
+        print("Keine Teamup-Dateien gefunden.")
+        return None
+
+    # Sortiere die Dateien basierend auf dem Zeitstempel im Namen
+    latest_file = sorted(teamup_files, key=lambda x: x.split('-')[1].split('.')[0], reverse=True)[0]
+
+    # Versuche, den Inhalt der neuesten Datei zu lesen
+    try:
+        with default_storage.open("teamup/" + latest_file, 'r') as file:
+            content = file.read()
+            # Konvertiere den JSON-String zurück in ein Python-Objekt
+            return json.loads(content)
+    except Exception as e:
+        print(f"Fehler beim Lesen der Datei {latest_file}: {e}")
+        return None
---
+++ roomsharing/bookings/management/commands/fetch_teamup.py
@@ -0,0 +1,125 @@
+import os
+import json
+import time
+
+from django.core.management.base import BaseCommand
+from dotenv import load_dotenv
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+
+
+
+## you can run this with python manage.py fetch_teamup
+## output gets saved to teamup_event_data_by_boopkinggroup.json in root
+## you ned TEAMUP_API_KEY in .env or add --key TEAMUP_API_KEY argument
+class Command(BaseCommand):
+    help = "fetch booking data of teamup of 2024 and sort it by bookinggroup"
+    load_dotenv()
+    teamup_api_key = os.getenv("TEAMUP_API_KEY", None)
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        # parser.add_argument("poll_ids", nargs="+", type=int)
+
+        # Named (optional) arguments
+        parser.add_argument(
+            "--key",
+            action="store",
+            help="Teamup API KEY",
+        )
+
+        parser.add_argument(
+            "--calendar",
+            action="store",
+            help="Teamup calendar Key orId",
+        )
+
+    def handle(self, *args, **options):
+        # ...
+        if options.get("key"):
+            self.teamup_api_key = options["key"]
+
+        if not options.get("calendar"):
+            raise ValueError("The --calendar tag is required.")
+
+        if self.teamup_api_key:
+            events2024 = self.get_teamup_events()
+            if type(events2024) == list:
+                self.stdout.write(f"fetched {len(events2024)} events")
+
+                ## sort events by series_id which is the same as bookinggroup
+                sorted_events = {}
+                new_series_id_counter = 1
+                for event in events2024:
+                    if event["series_id"] is None:
+                        event["series_id"] = new_series_id_counter
+                        new_series_id_counter += 1
+
+                    series_id = event["series_id"]
+                    sorted_events.setdefault(series_id, []).append(event)
+                
+                file_name = time.strftime("%Y%m%d-%H%M%S") + ".json"
+
+                try:
+                    sorted_events_json = json.dumps(sorted_events, ensure_ascii=False, indent=4)
+                    
+                    # Speichere den JSON-String in einer Datei.
+                    # Da default_storage.save einen Namen und ein ContentFile (oder ähnliches) erwartet,
+                    # verwenden wir ContentFile, um den JSON-String zu speichern.
+                    cachfile = default_storage.save(f"teamup/{file_name}", ContentFile(sorted_events_json.encode('utf-8')))
+                    
+                    self.stdout.write(self.style.SUCCESS(f"Successfully wrote to {file_name}"))
+                except Exception as e:
+                    self.stdout.write(self.style.ERROR(f"Error writing to {file_name}: {e}"))
+        else:
+            self.stdout.write("no Teamup Api Key")
+
+    def get_teamup_events(self):
+        import http.client
+        import json
+        import re
+
+        # Definiere die Verbindung und Header
+        conn = http.client.HTTPSConnection("api.teamup.com")
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Teamup-Token": self.teamup_api_key,
+        }
+
+        # Sende die Anfrage
+        conn.request(
+            "GET",
+            "/ksn1y3bu4h7uckv547/events?startDate=1.1.2024&endDate=31.12.2024",
+            headers=headers,
+        )
+
+        # Lese die Antwort
+        res = conn.getresponse()
+        data = res.read().decode("utf-8")
+        data = json.loads(data)
+        self.stdout.write(str(res.status))
+        # Überprüfe, ob ein Fehler aufgetreten ist
+        if res.status != 200 and res.status != 201:
+            # Parsen der Fehlermeldung
+            error_id = data["error"]["id"]
+            error_title = data["error"]["title"]
+            error_message = data["error"]["message"]
+            # Entferne HTML-Tags aus der Fehlermeldung
+            error_message_clean = re.sub(r"<.*?>", "", error_message)
+
+            # Ausgabe der Fehlerdetails
+            self.stdout.write("Error ID:", error_id)
+            self.stdout.write("Error Title:", error_title)
+            self.stdout.write("Error Message:", error_message_clean)
+            conn.close()
+            return False
+
+        else:
+            # Wenn keine Fehler auftreten, gib die Antwort in der Konsole aus
+            # print(data)
+            conn.close()
+            if "events" in data:
+                return data["events"]
+            else:
+                return False
