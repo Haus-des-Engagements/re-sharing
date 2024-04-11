0a80fcc072a4c09ac3cb414fb373756621ab7445
finnus <40977905+finnus@users.noreply.github.com>
2024-04-11 10:23:16 +0200

    tests: add tests

--- roomsharing/bookings/tests/test_views.py
+++ roomsharing/bookings/tests/test_views.py
@@ -3,28 +3,53 @@ from http import HTTPStatus
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.http import HttpResponseRedirect
+from django.test import Client
 from django.test import RequestFactory
+from django.test import TestCase
 from django.urls import reverse
 
+from roomsharing.bookings.tests.factories import BookingFactory
+from roomsharing.bookings.tests.factories import BookingGroupFactory
 from roomsharing.bookings.views import MyBookingsListView
-from roomsharing.users.models import User
+from roomsharing.users.tests.factories import OrganizationFactory
 from roomsharing.users.tests.factories import UserFactory
 
 
-class TestMyBookingListView:
-    def test_authenticated(self, user: User, rf: RequestFactory):
-        request = rf.get("/bookings/")
-        request.user = UserFactory()
-        response = MyBookingsListView.as_view()(request)
+class TestMyBookingListView(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = UserFactory()
 
+    def test_authenticated(self):
+        client = Client()
+        client.force_login(self.user)
+        response = client.get(reverse("bookings:my_bookings_list"))
         assert response.status_code == HTTPStatus.OK
 
-    def test_not_authenticated(self, user: User, rf: RequestFactory):
-        request = rf.get("/fake-url/")
+    def test_not_authenticated(self):
+        request = self.factory.get("/bookings/")
         request.user = AnonymousUser()
         response = MyBookingsListView.as_view()(request)
         login_url = reverse(settings.LOGIN_URL)
 
         assert isinstance(response, HttpResponseRedirect)
         assert response.status_code == HTTPStatus.FOUND
-        assert response.url == f"{login_url}?next=/fake-url/"
+        assert response.url == f"{login_url}?next=/bookings/"
+
+    def test_get_only_own_organization_bookings(self):
+        client = Client()
+        client.force_login(self.user)
+
+        o1 = OrganizationFactory()
+        self.user.organizations.set([o1])
+        bg = BookingGroupFactory(user=self.user, organization=o1)
+        BookingFactory(booking_group=bg)
+        BookingFactory(booking_group=bg)
+        total_bookings_for_o1 = 2
+        BookingFactory()
+
+        client.login(username=self.user.email, password=self.user.password)
+        response = client.get(reverse("bookings:my_bookings_list"))
+
+        assert response.status_code == HTTPStatus.OK
+        assert len(list(response.context["bookings_list"])) == total_bookings_for_o1
