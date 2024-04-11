0510af27ec0ee6d429c07b055c4c46fc5601400b
finnus <40977905+finnus@users.noreply.github.com>
2024-04-11 12:49:44 +0200

    feat: add very simple booking form

--- roomsharing/bookings/urls.py
+++ roomsharing/bookings/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .views import BookingCreateView
 from .views import BookingListView
 from .views import MyBookingsListView
 
@@ -7,4 +8,5 @@ app_name = "bookings"
 urlpatterns = [
     path("all/", BookingListView.as_view(), name="bookings_list"),
     path("", MyBookingsListView.as_view(), name="my_bookings_list"),
+    path("new/", BookingCreateView.as_view(), name="booking_create"),
 ]
