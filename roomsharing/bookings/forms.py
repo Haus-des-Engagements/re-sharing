7376879e3eec515470aaf6b1b1ba77aa213c2384
finnus <40977905+finnus@users.noreply.github.com>
2024-04-17 12:01:56 +0200

    feat: use FBC with optional initital params (startdate + duration)

--- roomsharing/bookings/forms.py
+++ roomsharing/bookings/forms.py
@@ -1,31 +1,102 @@
+import datetime
+
 from django import forms
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 from .models import Booking
 from .models import BookingGroup
 
 
 class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Booking
-        fields = ["timespan", "room"]
-
-    title = forms.CharField(label="Title")
+    startdate = forms.DateField(
+        label=_("Start Date"),
+        widget=forms.DateInput(attrs={"type": "date"}),
+    )
+    starttime = forms.ChoiceField(label=_("Start Time"))
+    duration = forms.ChoiceField(label=_("Duration (minutes)"))
+    organization = forms.ModelChoiceField(
+        queryset=None,
+        label=_("Organization"),
+    )  # Define the organization field
+    title = forms.CharField(label=_("Title"))
 
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["organization"] = forms.ModelChoiceField(
-            queryset=user.organizations.all(),
-            label="Organization",
-        )
+        self.fields["organization"].queryset = user.organizations.all()
+        self.fields["starttime"].choices = [
+            (time.strftime("%H:%M"), time.strftime("%H:%M"))
+            for time in (
+                datetime.datetime.combine(
+                    datetime.datetime.now(tz=datetime.UTC).date(),
+                    datetime.time(hour, minute),
+                )
+                for hour in range(6, 24)
+                for minute in (0, 30)
+            )
+        ]
 
-    def save(self):
+        # Generate choices for duration dropdown
+        self.fields["duration"].choices = [
+            (str(mins / 60), f"{mins / 60:.1f} hour(s)") for mins in range(30, 481, 30)
+        ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        room = cleaned_data.get("room")
+        startdate = cleaned_data.get("startdate")
+        starttime = cleaned_data.get("starttime")
+        duration = cleaned_data.get("duration")
+
+        if room and startdate and starttime:
+            starttime_as_datetime = datetime.datetime.strptime(
+                starttime,
+                "%H:%M",
+            ).astimezone(datetime.UTC)
+
+            if starttime_as_datetime.minute not in [0, 30]:
+                msg = _("Start time must be selected in 30-minute intervals.")
+                self.add_error("starttime", msg)
+
+            start_datetime = timezone.make_aware(
+                datetime.datetime.combine(
+                    startdate,
+                    starttime_as_datetime.time(),
+                ),
+            )
+
+            duration_float = float(duration)
+            if duration_float % 0.5 != 0:
+                msg = _("Duration must be selected in 30-minute intervals.")
+                self.add_error("duration", msg)
+
+            end_datetime = start_datetime + datetime.timedelta(hours=duration_float)
+
+            booking_overlap = Booking.objects.filter(
+                room=room,
+                timespan__overlap=(start_datetime, end_datetime),
+            )
+            if booking_overlap.exists():
+                msg = _("The room is already booked during your selected timeslot.")
+                self.add_error("room", msg)
+
+            cleaned_data["timespan"] = (start_datetime, end_datetime)
+
+        return cleaned_data
+
+    def save(self, user):
         booking = super().save(commit=False)
         booking_group = BookingGroup.objects.create(
             title=self.cleaned_data["title"],
-            user=booking.user,
+            user=user,
             organization=self.cleaned_data["organization"],
         )
         booking_group.save()
         booking.booking_group = booking_group
+        booking.timespan = self.cleaned_data["timespan"]
         booking.save()
         return booking
+
+    class Meta:
+        model = Booking
+        fields = ["room", "startdate", "starttime", "duration", "organization"]
--- roomsharing/bookings/urls.py
+++ roomsharing/bookings/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import BookingCreateView
 from .views import BookingListView
 from .views import MyBookingsListView
+from .views import create_booking
 
 app_name = "bookings"
 urlpatterns = [
     path("all/", BookingListView.as_view(), name="bookings_list"),
     path("", MyBookingsListView.as_view(), name="my_bookings_list"),
-    path("new/", BookingCreateView.as_view(), name="booking_create"),
+    path("new/", create_booking, name="booking_create"),
 ]
