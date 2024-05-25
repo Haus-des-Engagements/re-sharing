ee5ebdbf2cc81f5d1e696a8931d51c996864e3a3
finnus <40977905+finnus@users.noreply.github.com>
2024-05-25 14:23:57 +0200

    test: correct tests

--- roomsharing/bookings/tests/test_urls.py
+++ roomsharing/bookings/tests/test_urls.py
@@ -3,10 +3,5 @@ from django.urls import reverse
 
 
 def test_bookings_list():
-    assert reverse("bookings:bookings_list") == "/bookings/all/"
-    assert resolve("/bookings/all/").view_name == "bookings:bookings_list"
-
-
-def test_my_bookings_list():
-    assert reverse("bookings:my_bookings_list") == "/bookings/"
-    assert resolve("/bookings/").view_name == "bookings:my_bookings_list"
+    assert reverse("bookings:list") == "/bookings/"
+    assert resolve("/bookings/").view_name == "bookings:list"
