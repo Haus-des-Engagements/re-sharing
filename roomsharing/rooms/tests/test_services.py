5bbb37c7ff089423e78f0abc74a932fa05416697
finnus <40977905+finnus@users.noreply.github.com>
2024-07-24 12:52:34 +0200

    tests: add tests for rooms

--- roomsharing/rooms/tests/test_services.py
+++ roomsharing/rooms/tests/test_services.py
@@ -1,12 +1,10 @@
 from datetime import datetime
-from unittest.mock import patch
 from zoneinfo import ZoneInfo
 
 import pytest
 from django.test import TestCase
 from django.utils import timezone
 
-from roomsharing.bookings.models import Booking
 from roomsharing.bookings.tests.factories import BookingFactory
 from roomsharing.rooms.services import filter_rooms
 from roomsharing.rooms.services import get_weekly_bookings
@@ -15,15 +13,11 @@ from roomsharing.utils.models import BookingStatus
 
 
 class GetWeeklyBookingsTest(TestCase):
-    @patch.object(Booking.objects, "filter")
-    @patch.object(timezone, "now")
-    def test_empty_weekly_bookings(self, mock_now, mock_filter):
-        # Define the return values for the mock objects
-        mock_now.return_value = timezone.make_aware(timezone.datetime(2024, 7, 23))
-        mock_filter.return_value = Booking.objects.none()
+    def test_empty_weekly_bookings(self):
+        date = timezone.make_aware(timezone.datetime(2024, 7, 23))
 
         room = RoomFactory()
-        date_string = mock_now.return_value.strftime("%Y-%m-%d")
+        date_string = date.strftime("%Y-%m-%d")
         time_slots, weekdays = get_weekly_bookings(room.slug, date_string)
 
         # Check the length of returned lists
@@ -33,7 +27,7 @@ class GetWeeklyBookingsTest(TestCase):
         assert len(weekdays) == number_of_weekdays
 
         # Check some sample values
-        assert time_slots[0]["time"] == datetime(
+        assert time_slots[0]["time"] == timezone.datetime(
             2024, 7, 22, 8, 0, tzinfo=ZoneInfo(key="Europe/Berlin")
         )
         assert time_slots[0]["booked"] == [
@@ -53,30 +47,30 @@ class GetWeeklyBookingsTest(TestCase):
         for slot in time_slots:
             assert all(booked is False for booked in slot["booked"])
 
-    @patch.object(timezone, "now")
-    def test_some_bookings_exist(self, mock_now):
-        mock_now.return_value = timezone.make_aware(timezone.datetime(2024, 6, 5))
+    def test_some_bookings_exist(self):
         room = RoomFactory()
         booking1 = BookingFactory(
             room=room,
             status=BookingStatus.CONFIRMED,
             timespan=(
-                timezone.datetime(2024, 6, 5, 8, 0),
-                timezone.datetime(2024, 6, 5, 9, 0),
+                timezone.make_aware(timezone.datetime(2024, 6, 5, 8, 0)),
+                timezone.make_aware(timezone.datetime(2024, 6, 5, 9, 0)),
             ),
         )
         booking2 = BookingFactory(
             room=room,
             status=BookingStatus.CONFIRMED,
             timespan=(
-                timezone.datetime(2024, 6, 5, 18, 0),
-                timezone.datetime(2024, 6, 5, 22, 0),
+                timezone.make_aware(timezone.datetime(2024, 6, 5, 18, 0)),
+                timezone.make_aware(timezone.datetime(2024, 6, 5, 22, 0)),
             ),
         )
         booking1.save()
         booking2.save()
 
-        date_string = mock_now.return_value.strftime("%Y-%m-%d")
+        date = timezone.make_aware(timezone.datetime(2024, 6, 5))
+
+        date_string = date.strftime("%Y-%m-%d")
         time_slots, weekdays = get_weekly_bookings(room.slug, date_string)
 
         number_of_timeslots = 32
@@ -84,20 +78,17 @@ class GetWeeklyBookingsTest(TestCase):
         number_of_weekdays = 7
         assert len(weekdays) == number_of_weekdays
 
-        # Check that specific slots are booked
         assert (
             time_slots[0]["booked"][2] is True
         )  # This should be True because we booked the slot from 8:00 to 9:00
         assert (
-            time_slots[10]["booked"][2] is True
+            time_slots[10]["booked"][2] is False
         )  # This should be False because we did not book the slot
         assert (
             time_slots[20]["booked"][2] is True
         )  # This should be True because we booked the slot from 18:00 to 22:00
 
-    @patch.object(timezone, "now")
-    def test_without_date(self, mock_now):
-        mock_now.return_value = timezone.make_aware(timezone.datetime(2024, 6, 5))
+    def test_without_date(self):
         date_string = None
         room = RoomFactory()
         time_slots, weekdays = get_weekly_bookings(room.slug, date_string)
