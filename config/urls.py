be03e599dbce89093d9491d8fd4ab4241ff1b599
finnus <40977905+finnus@users.noreply.github.com>
2024-08-31 08:22:42 +0200

    refa: ad path url handling for media files

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include
+from django.urls import include, re_path
 from django.urls import path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
+from django.views.static import serve
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -27,6 +28,7 @@ urlpatterns = [
         include("roomsharing.organizations.urls", namespace="organizations"),
     ),
     # Media files
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
 
