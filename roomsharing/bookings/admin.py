94a77554f346e2d1c3ce0b048aab0c0da5f8f043
finnus <40977905+finnus@users.noreply.github.com>
2024-09-04 14:48:57 +0200

    refa: enable confirmation and cancellation in dj-admin

--- roomsharing/bookings/admin.py
+++ roomsharing/bookings/admin.py
@@ -1,5 +1,11 @@
 # Register your models here.
+from auditlog.context import set_actor
 from django.contrib import admin
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ngettext
+
+from roomsharing.utils.models import BookingStatus
 
 from .models import Booking
 from .models import BookingMessage
@@ -10,9 +16,46 @@ from .models import RecurrenceRule
 class BookingAdmin(admin.ModelAdmin):
     model = Booking
     list_display = ["id", "room", "timespan", "status", "organization", "title", "user"]
-    search_fields = ["id", "room", "organization", "title", "user"]
+    search_fields = ["id", "title"]
     list_filter = ["status", "organization", "room", "recurrence_rule"]
     ordering = ["id"]
+    actions = ["confirm_bookings", "cancel_bookings"]
+
+    @admin.action(description=_("Confirm selected bookings"))
+    def confirm_bookings(self, request, queryset):
+        for booking in queryset:
+            with set_actor(request.user):
+                booking.status = BookingStatus.CONFIRMED
+                booking.save()
+        count = queryset.count()
+        self.message_user(
+            request,
+            ngettext(
+                "%d booking was successfully confirmed.",
+                "%d bookings were successfully confirmed.",
+                count,
+            )
+            % count,
+            messages.SUCCESS,
+        )
+
+    @admin.action(description=_("Cancel selected bookings"))
+    def cancel_bookings(self, request, queryset):
+        for booking in queryset:
+            with set_actor(request.user):
+                booking.status = BookingStatus.CANCELLED
+                booking.save()
+        count = queryset.count()
+        self.message_user(
+            request,
+            ngettext(
+                "%d booking was successfully cancelled.",
+                "%d bookings were successfully cancelled.",
+                count,
+            )
+            % count,
+            messages.SUCCESS,
+        )
 
 
 admin.site.register(BookingMessage)
