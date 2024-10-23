710451b9c46ffd8993e4cc73135f3a1128060e25
finnus <40977905+finnus@users.noreply.github.com>
2024-10-23 10:32:45 +0200

    feat: send .ics with confirmation email

--- roomsharing/bookings/tasks.py
+++ roomsharing/bookings/tasks.py
@@ -1,18 +1,44 @@
 # tasks.py
+
+from django.contrib.sites.models import Site
+from django.core.mail import EmailMessage
 from django.core.mail import send_mail
+from django.utils import timezone
+from icalendar import Calendar
+from icalendar import Event
 
 from roomsharing.utils.models import BookingStatus
 
 
 def booking_confirmation_email(booking):
-    send_mail(
+    domain = Site.objects.get_current().domain
+    cal = Calendar()
+    event = Event()
+
+    event.add("uid", booking.uuid)
+    event.add("summary", booking.title)
+    event.add("dtstart", booking.timespan.lower)
+    event.add("dtend", booking.timespan.upper)
+    event.add("dtstamp", timezone.now())
+    event.add("location", booking.room.name)
+    event["description"] = (
+        f"Find all related information (access(ability), door code) and actions "
+        f"(cancel) here: {domain}{booking.get_absolute_url()}"
+    )
+    cal.add_component(event)
+
+    # Create an ICS file
+    ical_content = cal.to_ical()
+    ical_filename = f"booking_{booking.slug}.ics"
+    email = EmailMessage(
         "Booking Confirmed",
-        f"Your booking {booking.title} has been confirmed. You can see and change it "
-        f"here: {booking.get_absolute_url()}",
+        f"Your booking {booking.title} has been confirmed. You can see and "
+        f"change it here: {booking.get_absolute_url()}",
         "raum.app@haus-des-engagements.de",
         [booking.user.email],
-        fail_silently=False,
     )
+    email.attach(ical_filename, ical_content, "text/calendar")
+    email.send(fail_silently=False)
 
 
 def booking_reminder_email(booking_slug):
