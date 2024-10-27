2d58c4101150c8170ff8c2bcda089b39b519181b
finnus <40977905+finnus@users.noreply.github.com>
2024-10-27 16:24:04 +0100

    feat: add django-dbbackup

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,6 +85,7 @@ THIRD_PARTY_APPS = [
     "auditlog",
     "django_q",
     "tinymce",
+    "dbbackup",
 ]
 
 LOCAL_APPS = [
@@ -299,12 +300,5 @@ ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
 # ------------------------------------------------------------------------------
 AUDITLOG_DISABLE_REMOTE_ADDR = True
 
-
-# Protect user uploaded media files https://github.com/cobusc/django-protected-media/
-PROTECTED_MEDIA_ROOT = "%s/protected/" % BASE_DIR
-PROTECTED_MEDIA_URL = "/protected"
-PROTECTED_MEDIA_SERVER = "nginx"
-PROTECTED_MEDIA_LOCATION_PREFIX = "/internal"  # Prefix used in nginx config
-PROTECTED_MEDIA_AS_DOWNLOADS = (
-    False  # Controls inclusion of a Content-Disposition header
-)
+DBBACKUP_STORAGE = "django.core.files.storage.FileSystemStorage"
+DBBACKUP_STORAGE_OPTIONS = {"location": str(APPS_DIR / "backups")}
