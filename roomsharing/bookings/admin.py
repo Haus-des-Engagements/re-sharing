a4b6cfa6ad9bb6f5f3e61f4e50a8c58aee662013
finnus <40977905+finnus@users.noreply.github.com>
2024-12-12 12:15:22 +0100

    feat: add admin action "generate bookings" and "delete bookings" for recurrencerules, update last_occurence_date when rrule string is changed Closes #92

--- roomsharing/bookings/admin.py
+++ roomsharing/bookings/admin.py
@@ -1,8 +1,15 @@
+from datetime import UTC
+from datetime import datetime
+from datetime import time
+from datetime import timedelta
+
 from auditlog.context import set_actor
+from dateutil.rrule import rrulestr
 from django.contrib import admin
 from django.contrib import messages
 from django.db.models import Count
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import ngettext
@@ -13,6 +20,8 @@ from roomsharing.utils.models import BookingStatus
 from .models import Booking
 from .models import BookingMessage
 from .models import RecurrenceRule
+from .services_recurrences import generate_bookings
+from .services_recurrences import max_future_booking_date
 
 
 @admin.register(Booking)
@@ -75,6 +84,7 @@ class RecurrenceRuleAdmin(ImportExportMixin, admin.ModelAdmin):
     ]
     list_filter = ["status", "organization"]
     readonly_fields = ["booking_count_link"]
+    actions = ["generate_bookings", "delete_bookings"]
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
@@ -90,15 +100,56 @@ class RecurrenceRuleAdmin(ImportExportMixin, admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         if change:  # This ensures we're modifying an existing record
+            # Organization has changed. Update related bookings.
             previous = RecurrenceRule.objects.get(pk=obj.pk)
-            if previous.organization != obj.organization:
-                # Organization has changed. Update related bookings.
-                bookings = Booking.objects.filter(recurrence_rule=obj)
-                for booking in bookings:
-                    booking.organization = obj.organization
-                Booking.objects.bulk_update(bookings, ["organization"])
+            bookings = Booking.objects.filter(recurrence_rule=obj)
+
+            for booking in bookings:
+                booking.organization = obj.organization
+            Booking.objects.bulk_update(bookings, ["organization"])
+
+            if previous.rrule != obj.rrule:
+                if "COUNT" not in obj.rrule and "UNTIL" not in obj.rrule:
+                    obj.last_occurrence_date = None
+                else:
+                    obj.last_occurrence_date = list(rrulestr(obj.rrule))[-1]
 
         super().save_model(request, obj, form, change)
+
+    @admin.action(description=_("Generate bookings"))
+    def generate_bookings(self, request, queryset):
+        for rrule in queryset:
+            with set_actor(request.user):
+                max_booking_date = timezone.now().date() + timedelta(
+                    days=max_future_booking_date
+                )
+                start_new_bookings_at = timezone.now()
+
+                last_second = time(hour=23, minute=59, second=59)
+                end_new_bookings_at = datetime.combine(
+                    max_booking_date, last_second
+                ).astimezone(UTC)
+                bookings = generate_bookings(
+                    rrule, start_new_bookings_at, end_new_bookings_at
+                )
+                for current_booking in bookings:
+                    # Determine if the current booking stems from the same rrule and
+                    # thus should not be saved
+                    is_same_rrule_booking = (
+                        current_booking.status == BookingStatus.UNAVAILABLE
+                        and Booking.objects.filter(room=rrule.room)
+                        .filter(timespan__overlap=current_booking.timespan)
+                        .filter(recurrence_rule=rrule)
+                        .exists()
+                    )
+                    if not is_same_rrule_booking:
+                        current_booking.save()
+
+    @admin.action(description=_("Delete bookings"))
+    def delete_bookings(self, request, queryset):
+        for rrule in queryset:
+            with set_actor(request.user):
+                Booking.objects.filter(recurrence_rule=rrule).delete()
 
 
 @admin.register(BookingMessage)
